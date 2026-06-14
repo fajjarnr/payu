5b81bb4b9076159a9f1414ecb891a9f545070786
fajar <zfajart@gmail.com>
2026-06-14 03:39:58 +0000

    test(wallet): add missing ContractVerifierBase for Spring Cloud Contract

---
+++ backend/wallet-service/src/test/java/id/payu/wallet/contract/ContractVerifierBase.java
@@ -0,0 +1,25 @@
+package id.payu.wallet.contract;
+
+import io.restassured.module.mockmvc.RestAssuredMockMvc;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.contract.verifier.messaging.boot.AutoConfigureMessageVerifier;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@AutoConfigureMockMvc(addFilters = false)
+@AutoConfigureMessageVerifier
+@ActiveProfiles("test")
+public abstract class ContractVerifierBase {
+
+    @Autowired
+    protected MockMvc mockMvc;
+
+    @BeforeEach
+    void setUpSecurityContext() {
+        RestAssuredMockMvc.mockMvc(mockMvc);
+    }
+}
